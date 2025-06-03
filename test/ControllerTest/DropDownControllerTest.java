ce989eba359b16e0ed6187acd4172dc91d39a6e3
DarioBucciero <d.bucciero1@studenti.unisa.it>
2025-06-04 01:08:24 +0200

    Refactoring Test

--- test/ControllerTest/DropDownControllerTest.java
+++ test/ControllerTest/DropDownControllerTest.java
@@ -3,44 +3,64 @@ package ControllerTest;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import projectworkgroup6.Controller.CanvasController;
 import projectworkgroup6.Controller.DropDownController;
-
 import projectworkgroup6.Model.ColorModel;
-import projectworkgroup6.Model.Rectangle;
 import projectworkgroup6.Model.Shape;
+import projectworkgroup6.View.ShapeView;
 
 import static org.mockito.Mockito.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DropDownControllerTest {
 
-    private DropDownController dropDownController;
+    private DropDownController controller;
     private CanvasController mockCanvasController;
-    private Shape testShape;
+    private Shape mockShape;
 
     @BeforeEach
-    public void setUp() {
-        dropDownController = new DropDownController();
+    void setUp() {
+        controller = new DropDownController();
         mockCanvasController = mock(CanvasController.class);
-        dropDownController.setCanvasController(mockCanvasController);
+        controller.setCanvasController(mockCanvasController);
 
-        testShape = new Rectangle(0, 0, false, 20, 10,
-                new ColorModel(0, 0, 0, 1),      // border: black
-                new ColorModel(255, 255, 255, 1) // fill: white
-        );
-
-        dropDownController.setSelectedShapeForTest(testShape);
+        mockShape = mock(Shape.class);
+        when(mockShape.getBorder()).thenReturn(new ColorModel(0, 0, 0, 1.0));
+        when(mockShape.getFill()).thenReturn(new ColorModel(255, 255, 255, 1.0));
+        when(mockShape.getDim1()).thenReturn(100.0);
+        when(mockShape.getDim2()).thenReturn(50.0);
+        when(mockShape.getX()).thenReturn(100.0);
+        when(mockShape.getY()).thenReturn(100.0);
+        when(mockShape.getXc()).thenReturn(75.0);
+        when(mockShape.getYc()).thenReturn(75.0);
     }
 
     @Test
-    public void testModifyStrokeDelegatesToCanvasController() {
-        dropDownController.testModifyStrokeWithColor(Color.RED);
-        verify(mockCanvasController).onColorChanged(eq(Color.BLACK), eq(Color.RED));
+    void testModifyStrokeWithColorDelegatesToCanvasController() {
+        controller.setSelectedShapeForTest(mockShape);
+        Color newFill = Color.RED;
+
+        controller.testModifyStrokeWithColor(newFill);
+
+        verify(mockCanvasController).onColorChanged(any(), eq(newFill));
     }
 
     @Test
-    public void testModifyFillDelegatesToCanvasController() {
-        dropDownController.testModifyFillWithColor(Color.BLUE);
-        verify(mockCanvasController).onColorChanged(eq(Color.WHITE), eq(Color.BLUE));
+    void testModifyFillWithColorDelegatesToCanvasController() {
+        controller.setSelectedShapeForTest(mockShape);
+        Color newStroke = Color.BLUE;
+
+        controller.testModifyFillWithColor(newStroke);
+
+        verify(mockCanvasController).onColorChanged(eq(newStroke), any());
+    }
+
+    @Test
+    void testSetAndGetSavedView() {
+        ShapeView mockView = mock(ShapeView.class);
+        controller.setSavedView(mockView);
+        assertEquals(mockView, controller.getSavedView());
     }
 }
